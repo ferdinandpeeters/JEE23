a8cdbaf23417dc37d988777fd58bb6c28e905e10
Dries <djse@live.be>
2019-11-20 11:12:58 +0100

    rewrite report

--- CarRental-ejb/build/generated-sources/ap-source-output/rental/Car_.java
+++ CarRental-ejb/build/generated-sources/ap-source-output/rental/Car_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import rental.CarType;
 import rental.Reservation;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-13T18:16:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-17T23:19:42")
 @StaticMetamodel(Car.class)
 public class Car_ { 
 
